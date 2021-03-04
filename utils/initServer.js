618f283efc1f4ac0c026eaec0feda3ff6cf19231
Dnyaneshwar.Bhadane <dnyaneshwarbhadane0@gmail.com>
2021-03-04 18:19:54 +0530

    initial Commit

---
+++ utils/initServer.js
@@ -0,0 +1,11 @@
+let bodyParser = require('body-parser');
+
+let serverInitUpObject =  function serverConfig(app) {
+        app.use(bodyParser.urlencoded({extended:false}));
+        app.use(bodyParser.json({ type: 'application/*+json' }));
+        console.log("Server is Setup");
+
+}
+
+
+module.exports.serverConfig  = serverInitUpObject;
